332c124ccef1c3426e44c8e68e462996ff95fbd2
Parth Shukla <31998380+ParthShuklaa@users.noreply.github.com>
2022-11-05 12:13:37 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,13 @@
+x=str(5)
+X=20
+# python is case sensitive
+y=int(10)
+z=float(9)
+name="Future_College"#string type
+city='Bareilly Smart City'
+
+print(y)
+print(z)
+print(X)
+print(name)
+print(city)
